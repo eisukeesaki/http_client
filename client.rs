0d2fa5546347d4688511fc679c34092307aaeabd
Eisuke Esaki <eisukeesaki@gmail.com>
2025-08-11 17:18:38 -0700

    construct socket out of user arguments

--- client.rs
+++ client.rs
@@ -1,8 +1,18 @@
 use std::net::TcpStream;
 
 fn main() {
-    let socket = "127.0.0.1:4242";
-    if let Ok(_) = TcpStream::connect(socket) {
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() != 3 {
+        println!("Terminating due to insufficent number of arguments.");
+        println!("usage: ./app <ipaddr> <port>");
+        std::process::exit(1);
+    }
+
+    let ipaddr = &args[1];
+    let port = &args[2];
+    let socket = format!("{}:{}", ipaddr, port);
+    if let Ok(_) = TcpStream::connect(&socket) {
         println!("Connected to {}", socket);
     }
     else {
