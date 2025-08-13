d709687a2386efd09eefdd8f28f0c556169cd1f9
Eisuke Esaki <eisukeesaki@gmail.com>
2025-08-13 00:20:43 -0700

    print response from server on console

--- client.rs
+++ client.rs
@@ -1,4 +1,6 @@
 use std::net::TcpStream;
+use std::io::Read;
+use std::process;
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
@@ -6,17 +8,29 @@ fn main() {
     if args.len() != 3 {
         println!("Terminating due to insufficent number of arguments.");
         println!("usage: ./app <ipaddr> <port>");
-        std::process::exit(1);
+        process::exit(1);
     }
 
     let ipaddr = &args[1];
     let port = &args[2];
     let socket = format!("{}:{}", ipaddr, port);
-    if let Ok(_) = TcpStream::connect(&socket) {
-        println!("Connected to {}", socket);
-    }
-    else {
-        println!("Couldn't connect to {}", socket);
+
+    match TcpStream::connect(&socket) {
+        Ok(mut stream) => {
+            let mut buff = [0; 1024];
+            match stream.read(&mut buff) {
+                Ok(bytes_read) => {
+                    let response = String::from_utf8_lossy(&buff[0..bytes_read]);
+                    println!("{}", response);
+                },
+                Err(e) => { // not tested
+                    eprintln!("Couldn't read from stream. {}", e);
+                }
+            }
+        },
+        Err(e) => { // not tested
+            println!("Couldn't connect to {}. {}", socket, e);
+        }
     }
 }
 
