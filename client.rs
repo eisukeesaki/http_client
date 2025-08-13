6364c44648c3083e902ded62ba4b2ccecbe1f8cf
Eisuke Esaki <eisukeesaki@gmail.com>
2025-08-13 13:39:28 -0700

    user can send message to server through stdin

--- client.rs
+++ client.rs
@@ -1,6 +1,7 @@
 use std::net::TcpStream;
 use std::io::Read;
 use std::process;
+use std::io::Write;
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
@@ -17,14 +18,30 @@ fn main() {
 
     match TcpStream::connect(&socket) {
         Ok(mut stream) => {
-            let mut buff = [0; 1024];
-            match stream.read(&mut buff) {
-                Ok(bytes_read) => {
-                    let response = String::from_utf8_lossy(&buff[0..bytes_read]);
-                    println!("{}", response);
-                },
-                Err(e) => { // not tested
-                    eprintln!("Couldn't read from stream. {}", e);
+            loop {
+                let mut buff = [0; 1024];
+                match stream.read(&mut buff) {
+                    Ok(bytes_read) => {
+                        let response =
+                            String::from_utf8_lossy(&buff[0..bytes_read]);
+                        println!("{}", response);
+
+                        println!("[Your message]");
+                        let mut user_input = String::new();
+                        match std::io::stdin().read_line(&mut user_input) {
+                            Ok(_) => {
+                                match stream.write_all(user_input.as_bytes()) {
+                                    Ok(_) => println!("Sent message to server."),
+                                    Err(e) => eprintln!("Failed to send \
+                                        messgage to server. {}", e),
+                                }
+                            },
+                            Err(e) => println!("Failed to read from stdin {}", e),
+                        }
+                    },
+                    Err(e) => { // not tested
+                        eprintln!("Couldn't read from stream. {}", e);
+                    }
                 }
             }
         },
