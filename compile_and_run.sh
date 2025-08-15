5802ca47a2b459e2d1f66d5e656936a96a6d3e3b
Eisuke Esaki <eisukeesaki@gmail.com>
2025-08-14 19:34:03 -0700

    add debug mode

--- compile_and_run.sh
+++ compile_and_run.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
-rustc client.rs -o client
+
+echo "rustc client.rs --cfg 'feature=\"debug\"' -o client"
+rustc client.rs --cfg 'feature="debug"' -o client
+echo 'RUNNING IN DEBUG MODE'
+
+# echo "rustc client.rs -o client"
+# rustc client.rs  -o client
+
+echo './client 127.0.0.1 4242'
 ./client 127.0.0.1 4242
